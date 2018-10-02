44c921aaa86f53947a8c49467cf97661665c2fdc
George Daramouskas <gdaramouskas@therp.nl>
2018-10-02 16:13:11 +0200

    [MIG] Migrate to new api

---
+++ account_asset_management/models/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import account_asset_invoice
+from . import account_asset
+from . import account_move
+from . import account
+from . import res_config
+from . import account_invoice_line
+from . import account_asset_category
+from . import account_asset_recompute_trigger
+from . import account_asset_asset
+from . import account_asset_depreciation_line
+from . import account_asset_history
